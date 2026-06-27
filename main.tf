3c2d2d389c035ca32342f3d5e6f2635c801e0b51
Syed Kashif Ali Ashraf Ali <kashifsayyad09@gmail.com>
2026-06-27 11:27:33 +0530

    Change EC2 instance type from t2.small to t3.micro

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "web-server" {
   ami           = "ami-08f44e8eca9095668"
-  instance_type = "t2.small"
+  instance_type = "t3.micro"
   tags = {
     Name = "Terraform-EC2"
   }
